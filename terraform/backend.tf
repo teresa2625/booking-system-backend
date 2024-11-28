46121d7ea7ac78d7d9a8cc51ac26d2e4dab28562
Teresa <chua0033@student.monash.edu>
2024-11-28 15:13:56 +1100

    different bucket

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-bucket"                    # Replace with your S3 bucket name
+    bucket         = "bs-terraform-state-bucket"                 # Replace with your S3 bucket name
     key            = "projects/booking-system/terraform.tfstate" # Path where the state file will be stored inside the bucket
     region         = "ap-southeast-2"                            # Replace with your AWS region
     encrypt        = true
