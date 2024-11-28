7782511775b3bd1312579043563d6a68b53e2876
Teresa <chua0033@student.monash.edu>
2024-11-28 19:11:59 +1100

    rename

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -4,6 +4,6 @@ terraform {
     key            = "projects/booking-system/terraform.tfstate" # Path where the state file will be stored inside the bucket
     region         = "ap-southeast-2"                            # Replace with your AWS region
     encrypt        = true
-    dynamodb_table = "terraform-lock-table" # DynamoDB table for state locking
+    dynamodb_table = "bs-terraform-lock-table" # DynamoDB table for state locking
   }
 }
--- terraform/main.tf
+++ terraform/main.tf
@@ -8,7 +8,7 @@ resource "aws_s3_bucket" "terraform_state" {
 }
 
 resource "aws_dynamodb_table" "terraform_lock" {
-  name         = "terraform-lock-table"
+  name         = "bs-terraform-lock-table"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
   attribute {
