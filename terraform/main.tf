102fa64f87d056e968c8da9ec7e35a0dd6dddf45
Teresa <chua0033@student.monash.edu>
2024-11-28 20:51:28 +1100

    test

--- terraform/main.tf
+++ terraform/main.tf
@@ -120,28 +120,28 @@ resource "aws_iam_policy" "backend_role_policy" {
   })
 }
 
-resource "aws_iam_role" "oidc_role" {
-  name = "OIDC_role"
+# resource "aws_iam_role" "oidc_role" {
+#   name = "OIDC_role"
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Principal = {
-          Federated = "arn:aws:iam::713292987965:oidc-provider/token.actions.githubusercontent.com"
-        },
-        Action = "sts:AssumeRoleWithWebIdentity",
-        Condition = {
-          StringLike = {
-            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com",
-            "token.actions.githubusercontent.com:sub" = "repo:teresa2625/booking-system-backend:*"
-          }
-        }
-      }
-    ]
-  })
-}
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17",
+#     Statement = [
+#       {
+#         Effect = "Allow",
+#         Principal = {
+#           Federated = "arn:aws:iam::713292987965:oidc-provider/token.actions.githubusercontent.com"
+#         },
+#         Action = "sts:AssumeRoleWithWebIdentity",
+#         Condition = {
+#           StringLike = {
+#             "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com",
+#             "token.actions.githubusercontent.com:sub" = "repo:teresa2625/booking-system-backend:*"
+#           }
+#         }
+#       }
+#     ]
+#   })
+# }
 
 resource "aws_cloudwatch_log_group" "BS_backend_log_group" {
   name              = "booking-system-backend-log-group"
