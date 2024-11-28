bd8bc54f75dd5c255c36f652c0acf963667c015c
Teresa <chua0033@student.monash.edu>
2024-11-28 14:30:27 +1100

    tfstate

---
+++ terraform/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-bucket"                    # Replace with your S3 bucket name
+    key            = "projects/booking-system/terraform.tfstate" # Path where the state file will be stored inside the bucket
+    region         = "ap-southeast-2"                            # Replace with your AWS region
+    encrypt        = true
+    dynamodb_table = "terraform-lock-table" # DynamoDB table for state locking
+  }
+}
