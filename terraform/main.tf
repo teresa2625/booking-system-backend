55737d161eca74e05e9e6134dfc3987466a2e597
Teresa <chua0033@student.monash.edu>
2024-11-28 19:15:43 +1100

    remove region

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,7 +4,6 @@ provider "aws" {
 
 resource "aws_s3_bucket" "terraform_state" {
   bucket = "bs-terraform-state-bucket" # Replace with a unique name
-  region = "ap-southeast-2"
 }
 
 resource "aws_dynamodb_table" "terraform_lock" {
